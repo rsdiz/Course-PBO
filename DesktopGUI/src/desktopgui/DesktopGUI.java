91f7f2ba054ab81c2705fba84eb3164dcffb888c
rsdiz <rosyid.iz.rsd@gmail.com>
2019-12-02 22:19:05 +0700

    Upload Source Code Java Desktop GUI (Mahasiswa)

---
+++ DesktopGUI/src/desktopgui/DesktopGUI.java
@@ -0,0 +1,19 @@
+package desktopgui;
+
+/**
+ *
+ * @author Dalpan
+ */
+public class DesktopGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Mahasiswa panelMhs = new Mahasiswa();
+        panelMhs.setVisible(true);
+        
+    }
+    
+}
