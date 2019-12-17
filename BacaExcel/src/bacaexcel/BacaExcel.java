22889e56e03005015ee96cc151e45d0ca48bbe82
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2019-12-17 22:27:26 +0700

    Upload Bab 12
    Membaca File dari Microsoft Excel

---
+++ BacaExcel/src/bacaexcel/BacaExcel.java
@@ -0,0 +1,18 @@
+package bacaexcel;
+
+/**
+ *
+ * @author  Muhammad Rosyid Izzulkhaq
+ * @nim     5180411122
+ */
+public class BacaExcel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormBacaExcel form = new FormBacaExcel();
+        form.setVisible(true);
+    }
+    
+}
