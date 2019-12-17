78867a71e477e5890df628372d426ee8a981845f
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2019-12-17 22:18:14 +0700

    Upload Bab 13 (Java Class Library)
    Simple Calculator

---
+++ SimpleCalculator with Java Class Library/Core/src/core/Calculator.java
@@ -0,0 +1,24 @@
+package core;
+
+/**
+ *
+ * @author Muhammad Rosyid Izzulkhaq
+ */
+public class Calculator {
+    // PENJUMLAHAN
+    public static double sum(double firstNumber, double secondNumber){
+        return firstNumber + secondNumber;
+    }
+    // PENGURANGAN
+    public static double subtract(double firstNumber, double secondNumber){
+        return firstNumber - secondNumber;
+    }
+    // PERKALIAN
+    public static double multiple(double firstNumber, double secondNumber){
+        return firstNumber * secondNumber;
+    }
+    // PEMBAGIAN
+    public static double divide(double firstNumber, double secondNumber){
+        return firstNumber / secondNumber;
+    }
+}
---
+++ SimpleCalculator with Java Class Library/SimpleCalculator/src/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,17 @@
+package simplecalculator;
+
+/**
+ *
+ * @author Muhammad Rosyid Izzulkhaq
+ */
+public class SimpleCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SimpleCalculatorFrame frame = new SimpleCalculatorFrame();
+        frame.setVisible(true);
+    }
+    
+}
